1f417df15cf15963a4d824c634cc4bf61b2c4c9f
Nikhil Yadav <masteringprogramming2021@gmail.com>
2025-08-13 09:56:16 +0530

    resolving issues

--- app/src/main/java/com/nikhil/wakeme/ui/screens/AlarmListScreen.kt
+++ app/src/main/java/com/nikhil/wakeme/ui/screens/AlarmListScreen.kt
@@ -22,6 +22,7 @@ import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.*
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AlarmListScreen(nav: NavController) {
     val context = LocalContext.current
